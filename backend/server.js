ce3785c8d2bdcc92ca3ae9f9b71f8f4cb304dbef
EdwinGuerreroCortez <20221030@uthh.edu.mx>
2024-05-08 08:51:22 -0600

    estadia

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const PORT = 5000;
 app.get('/', (req, res) => {
     res.send('Bienvenido al backend de UniGame!');
 });
+//dasa
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
